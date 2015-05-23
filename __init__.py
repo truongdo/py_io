487529f0011c76e34fcea13b2f1070de96abf267
truong-d <do.q.truong@gmail.com>
2015-05-24 00:05:17 +0900

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 truong-d <truong-d@truongd-XPS13-9333>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+all = [py_io, py_log]
